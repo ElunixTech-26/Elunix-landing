2447072ed949a57c0d4cf6a6717ba5ec4aac946c
antonio-mauricio-1694 <antoniomauriciodev1694@gmail.com>
2026-01-11 03:11:09 -0300

    feat:criando header eo herro  responsivo com menu hamburguer e hero adaptado

---
+++ components/hero/hero.js
@@ -0,0 +1,22 @@
+const card = document.querySelector('.hero-card');
+
+if (card) {
+  card.addEventListener('mousemove', (e) => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const rotateY = (x / rect.width - 0.5) * 12;
+    const rotateX = (y / rect.height - 0.5) * -12;
+
+    card.style.transform = `
+      rotateX(${rotateX}deg)
+      rotateY(${rotateY}deg)
+      translateY(-10px)
+    `;
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.style.transform = '';
+  });
+}
